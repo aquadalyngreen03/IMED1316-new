41df033e722aace0fc00d52973356e49520e0963
aquadalyngreen03 <aquadalyngreen03@gmail.com>
2026-04-27 15:00:57 -0500

    push

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 //document.querySelecter('my vid').defaultPlayRate = 2;
 
 ScrollReveal().reveal('.headline');
+
+<script
+            src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.9.1/tsparticles.confetti.bundle.min.js"></script>

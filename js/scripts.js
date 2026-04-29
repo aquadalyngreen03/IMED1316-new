fb156afda819ef5525bbf3bcd6f533c4670d3695
aquadalyngreen03 <aquadalyngreen03@gmail.com>
2026-04-29 18:18:14 -0500

    push

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,12 @@
 
 ScrollReveal().reveal('.headline');
 
-confetti({
-  particleCount: 100,
-  spread: 70,
-  origin: { y: 0.6 }
-});
+const defaults = {
+  spread: 360,
+  ticks: 50,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+  shapes: ['star'],
+  colors: ['FFE400', 'FFBD00', 'E89400', 'FFCA6C', 'FDFFB8'],
+};
